8358df7e07c0bdb1101a5e3995f311a9a2ea72df
NilsFC <nils.ciancio@gmail.com>
2025-08-12 12:22:29 +0200

    v0.3 schematics + v23 firmware;

---
+++ Software/zmk-model_x_23/output/Compile v23.bat
@@ -0,0 +1,4 @@
+cd %HOMEPATH%\git\zmk\app\
+west build --pristine -b nice_nano_v2 -S studio-rpc-usb-uart -- -DSHIELD=model_x -DZMK_EXTRA_MODULES="c:/Users/nilsc/git/zmk-model_x_23/"
+copy ..\..\zmk\app\build\zephyr\zmk.uf2 c:\Users\nilsc\git\zmk-model_x_23\output\ZMK-Model_X_v23.uf2
+pause
